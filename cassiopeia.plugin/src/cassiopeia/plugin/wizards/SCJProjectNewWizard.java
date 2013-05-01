94952fda6421acdee9072961cb751abccd197ba2
Todberg <mtodbe08@student.aau.dk>
2013-05-01 18:38:59 +0200

    Wizard creation...

---
+++ cassiopeia.plugin/src/cassiopeia/plugin/wizards/SCJProjectNewWizard.java
@@ -0,0 +1,37 @@
+package cassiopeia.plugin.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+public class SCJProjectNewWizard extends Wizard implements INewWizard {
+
+	private WizardNewProjectCreationPage pageOne;
+	
+	public SCJProjectNewWizard() {
+		setWindowTitle("New SCJ Project");
+	}
+	
+	@Override
+	public void addPages() {
+		super.addPages();
+		
+		pageOne = new WizardNewProjectCreationPage("Page one");
+		pageOne.setTitle("Create a SCJ Project");
+		pageOne.setDescription("Enter a project name.");
+		
+		addPage(pageOne);
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		return true;
+	}
+}
